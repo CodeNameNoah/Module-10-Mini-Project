85fdc1618ba0da3695e20a09a665af69dbba506e
Noah Hoang <hoincbusiness@gmail.com>
2023-04-21 14:28:50 -0700

    Revised document.js template literal to include modified title

--- Develop/lib/document.js
+++ Develop/lib/document.js
@@ -6,13 +6,14 @@ function createDocument(title, tasks = []) {
   // TODO: Add TaskListItems to a new TaskList
 
   // TODO: Modify the template below to include your title, Header, and TaskList
+  // ? Title ✅
   return `<!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>YOUR TITLE HERE</title>
+      <title>${title}</title>
       <link rel="stylesheet" href="../dist/style.css" />
     </head>
     <body>
